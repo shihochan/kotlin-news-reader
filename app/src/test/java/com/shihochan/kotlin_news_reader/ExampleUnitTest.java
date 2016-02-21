07a734d0db7da92a7ef5217c940d3e00601ed51f
shiho_yuki <shiho_yuki@cyberagent.co.jp>
2016-02-21 19:03:37 +0900

    Created empty application

---
+++ app/src/test/java/com/shihochan/kotlin_news_reader/ExampleUnitTest.java
@@ -0,0 +1,16 @@
+package com.shihochan.kotlin_news_reader;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
