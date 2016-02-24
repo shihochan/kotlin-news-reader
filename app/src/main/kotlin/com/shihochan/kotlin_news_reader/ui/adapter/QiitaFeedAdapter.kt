4263eb83038817a6461af206609a0bde4cc32003
shihochan <yuki.shiho.dev@gmail.com>
2016-02-25 00:34:05 +0900

    Added adapter, binder, custom view and layout

---
+++ app/src/main/kotlin/com/shihochan/kotlin_news_reader/ui/adapter/QiitaFeedAdapter.kt
@@ -0,0 +1,26 @@
+package com.shihochan.kotlin_news_reader.ui.adapter
+
+import com.shihochan.kotlin_news_reader.model.dto.QiitaDto
+import com.shihochan.kotlin_news_reader.ui.adapter.binder.FeedOneColumnBinder
+import com.yqritc.recyclerviewmultipleviewtypesadapter.ListBindAdapter
+
+/**
+ * Created by Yuki Shiho on 2016/02/24.
+ */
+class QiitaFeedAdapter : ListBindAdapter() {
+
+    private val binder: FeedOneColumnBinder
+        get() = getDataBinder(0)
+
+    init {
+        addAllBinder(FeedOneColumnBinder(this))
+    }
+
+    fun addAll(contents: List<QiitaDto>) {
+        binder.addAll(contents)
+    }
+
+    fun clearAll() {
+        binder.clear()
+    }
+}
